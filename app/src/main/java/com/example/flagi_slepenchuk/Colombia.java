b79a023b6158dc77b01c4b0668b0ea5761288343
Slepenchuk <74017330+Slepenchuk@users.noreply.github.com>
2020-11-15 17:39:31 +0300

    Флаги - Слепенчук

---
+++ app/src/main/java/com/example/flagi_slepenchuk/Colombia.java
@@ -0,0 +1,14 @@
+package com.example.flagi_slepenchuk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class Colombia extends Activity {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.colombia);
+    }
+
+}
